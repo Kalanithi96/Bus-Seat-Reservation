0f50f27413c43a6809b835b8084d6eb1e7074d5b
Kalanithi96 <kalanithi96@gmail.com>
2023-02-04 23:53:34 +0530

    Inserted schema for Bus Seating arrangement

--- FrontEnd/src/pages/Bus.jsx
+++ FrontEnd/src/pages/Bus.jsx
@@ -1,15 +1,115 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Header1 from "../Components/Header/Header1";
 import Footer from "../Components/Footer/Footer";
 import "./bus/bus.css";
 
 const Bus = () => {
+    const navigate = useNavigate();
+
+    const previewButtonHandler = () =>{
+        navigate( "/passengerDetails" )
+    }
+
     return (
         <div>
             <Header1/>
                 <div className="big_container">
                     <div className="bus_container">
-                        
+                        <div className="bus">
+                            <div className="left">
+                                <div>
+                                    <button>-</button>
+                                </div>
+                                <div>
+                                    <button>A1</button>
+                                </div>
+                                <div>
+                                    <button>B1</button>
+                                </div>
+                                <div>
+                                    <button>C1</button>
+                                </div>
+                                <div>
+                                    <button>D1</button>
+                                </div>
+                                <div>
+                                    <button>E1</button>
+                                </div>
+                                <div>
+                                    <button>F1</button>
+                                </div>
+                                <div>
+                                    <button>G1</button>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div>
+                                    <span>
+                                        <button>--</button>
+                                    </span>
+                                    <span>
+                                        <button>--</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>A2</button>
+                                    </span>
+                                    <span>
+                                        <button>A3</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>B2</button>
+                                    </span>
+                                    <span>
+                                        <button>B3</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>C2</button>
+                                    </span>
+                                    <span>
+                                        <button>C3</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>D2</button>
+                                    </span>
+                                    <span>
+                                        <button>D3</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>E2</button>
+                                    </span>
+                                    <span>
+                                        <button>E3</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>F2</button>
+                                    </span>
+                                    <span>
+                                        <button>F3</button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        <button>G2</button>
+                                    </span>
+                                    <span>
+                                        <button>G3</button>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="passenger_container">
                         <div className="ind_pass">
@@ -43,6 +143,39 @@ const Bus = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="ind_pass">
+                            <div className="ind_pas_upper">
+                                <div>
+                                    Passenger 2
+                                </div>
+                                <div>
+                                    Name
+                                </div>
+                                <div>
+                                    <input type="text" placeholder="Name"/>
+                                </div>
+                            </div>
+                            <div className="ind_pas_lower">
+                                <div>
+                                    <div>
+                                        Age
+                                    </div>
+                                    <div>
+                                        <input type="number" placeholder="Age"/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>
+                                        Gender
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Gender"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <div className="contact_container">
                         <div>
@@ -62,7 +195,7 @@ const Bus = () => {
                             </div>
                         </div>
                         <div>
-                            <button type="submit">Preview</button>
+                            <button type="submit" onClick={previewButtonHandler}>Preview</button>
                         </div>
                     </div>
                 </div>
